dbdd4a93b85be82726db415f17625ac238b884b1
Juan Benedit <juan@kaixo.agency>
2025-03-05 17:15:18 -0500

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -285,10 +285,6 @@ $(document).ready(function () {
         $cursor.removeClass("cursor-white");
     });
 
-    $(".nav-light").on("mouseleave", function () {
-        $cursor.removeClass("cursor-white");
-    });
-
     // Invert text color only inside .section_case-studies
     $(".section_case-studies a, .section_case-studies button").on("mouseenter", function () {
         $(this).addClass("text-invert");
