8839b56aa9030a1db283dda5dec623cb1e57d59c
Juan Benedit <juan@kaixo.agency>
2025-03-04 21:54:57 -0500

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -330,7 +330,7 @@ $(document).ready(function () {
 
     // When hovering over .tool-icon, add the 'tooltip' class to .custom-cursor and hide the system cursor
     $(".navbar3_logo-link").on("mouseenter", function () {
-        $(".custom-cursor").addClass("tooltip, avatar");
+        $(".custom-cursor").addClass("tooltip avatar");
         $(".cursor-carat").css("visibility", "visible");  // Make the .cursor-carat visible
         $(".cursor-avatar").css("visibility", "visible");  // Make the .cursor-avatar visible
         $(this).css("cursor", "none");  // Hide the system cursor
