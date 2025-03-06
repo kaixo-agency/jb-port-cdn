efdd03f46e89f252c2f11bb1b41843bbe9a964ba
Juan Benedit <juan@kaixo.agency>
2025-03-06 16:52:16 -0500

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -349,11 +349,11 @@ $(document).ready(function () {
     // When hovering over .navbar item, add the 'down arrow' class to .custom-cursor and hide the system cursor
     $(".navbar3_link").on("mouseenter", function () {
         $(".custom-cursor").addClass("down-arrow");
-        $("down-arrow").css("display", "block");  // Show the arrow
+        $(".cursor-arrow-down").css("display", "block");  // Show the arrow
         $(this).css("cursor", "none");  // Hide the system cursor
     }).on("mouseleave", function () {
         $(".custom-cursor").removeClass("down-arrow");
-        $("down-arrow").css("display", "none");  // Hide the arrow
+        $(".cursor-arrow-down").css("display", "none");  // Hide the arrow
         $(this).css("cursor", "");  // Restore the default system cursor
     });
 
