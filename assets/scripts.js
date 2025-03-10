ca85e6dfdada77c57683bb3fc7d3aad9a35a234f
Juan Benedit <juan@kaixo.agency>
2025-03-10 18:19:56 -0400

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -372,6 +372,5 @@ $(document).ready(function () {
                 $video.play();
             }, 1000);
         });
-        $(".custom-cursor").removeClass("tooltip cursor-text-visible");
     });
 });
