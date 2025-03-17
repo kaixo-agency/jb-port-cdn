1416a0706324e37987aeccf56f422b1d0ee44563
Juan Benedit <juan@kaixo.agency>
2025-03-17 18:30:25 -0400

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -114,8 +114,7 @@ $(document).ready(function () {
             // Apply dark mode
             navbar.addClass("nav-dark"); 
             navbar.css({
-                "background": "var(--base-color-brand--slate-dark)", // Updated dark mode background
-                "border-color": "var(--base-color-brand--slate-dark)", 
+                "background": "var(--base-color-brand--slate-medium)", // Updated dark mode background
                 "transition": "background-color 0.5s ease, border-color 0.5s ease",
                 "box-shadow": "0 4px 20px #4b5f7240"
             });
