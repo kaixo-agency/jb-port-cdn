4f263d6ccde122cbb5b0496bbdd85f1cf55fbdd3
Juan Benedit <juan@kaixo.agency>
2025-04-08 09:22:57 -0400

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
             "transition": "color 0.5s ease"
         });
         logoSvgPaths.css({
-            "stroke": "var(--base-color-brand--slate-medium)",
+            "stroke": "#21233A",
             "transition": "stroke 0.5s ease"
         });
         buttons.css({
