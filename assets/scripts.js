5f1c4d153989bf18bd4053e9db98e29fc5add9ab
Juan Benedit <juan@kaixo.agency>
2025-05-04 16:13:25 -0400

    Auto-commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -346,12 +346,12 @@ $(document).ready(function () {
 $(document).ready(function () {
     // When hovering over 
     $(".has-video").on("mouseenter", function () {
-        $(".custom-cursor").addClass("tooltip");
+        $(".custom-cursor").addClass("play");
         //$(".cursor-carat").css("visibility", "visible");  // Make the .cursor-carat visible
         $(".cursor-play").css("visibility", "visible");  // Hide the .cursor-carat again
         $(this).css("cursor", "none");  // Hide the system cursor
     }).on("mouseleave", function () {
-        $(".custom-cursor").removeClass("tooltip");
+        $(".custom-cursor").removeClass("play");
         //$(".cursor-carat").css("visibility", "hidden");  // Hide the .cursor-carat again
         $(this).css("cursor", "");  // Restore the default system cursor
     });
